4088c03671bc77207dbb85a4465d041331a39f55
Karthick Babu <djkarthick@gmail.com>
2021-05-05 06:34:36 +0530

    Removed used ports

--- src/TestCases/function.java
+++ src/TestCases/function.java
@@ -1,15 +1,7 @@
 package TestCases;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pages.pageobjects;
 import reusables.browser;
